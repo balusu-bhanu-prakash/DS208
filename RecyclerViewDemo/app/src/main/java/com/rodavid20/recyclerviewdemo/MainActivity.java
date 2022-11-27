1ec7510f0b65beab2ac8e2ab929aa7acd7fe5369
balusubhanuprakash <balusubhanuprakash091@gmail.com>
2022-11-27 11:40:37 +0530

    c1

--- RecyclerViewDemo/app/src/main/java/com/rodavid20/recyclerviewdemo/MainActivity.java
+++ RecyclerViewDemo/app/src/main/java/com/rodavid20/recyclerviewdemo/MainActivity.java
@@ -2,7 +2,11 @@ package com.rodavid20.recyclerviewdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent();
+        EditText emailtext = findViewById(R.id.etEmail);
+        EditText passwtext = findViewById(R.id.etPassword);
+        Button signin = findViewById(R.id.btnLogin);
+        Button reg = findViewById(R.id.btnRegister);
+        if (emailtext.getText().toString() == "" | passwtext.getText().toString() == "") {
+            signin.setOnClickListener(view -> Toast.makeText(this, "Please fill the empty fileds", Toast.LENGTH_SHORT).show());
+        }
+        else {
+
+        }
     }
 }
